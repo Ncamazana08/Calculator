d093b8a0785b5d089b8fd01c64f90e75820f7b50
Ncamazana08 <siyandancamazana08@gmail.com>
2021-11-23 14:40:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let sum = "";
+function addToSum(operator){
+    sum = sum + operator;
+    document.getElementById("output").innerText = sum;
+}
+function evaluteSum(){
+    document.getElementById("output").innerText = eval(sum);
+}
+function clearSum(operator){
+    sum = "";
+    document.getElementById("output").innerText = sum;
+}
+
+function backspace() {
+    sum = sum.slice(0,-1);
+    document.getElementById("output").innerText = sum;
+}
